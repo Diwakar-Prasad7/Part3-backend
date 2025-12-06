a9ef79094c25809bea8c7cc7335d153c90fbf355
Diwakar <diwakar@Diwakar.Diwakar>
2025-12-06 16:15:54 +0530

    Backend deployed and tested on Render

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 morgan.token('body', (req) => (req.method === 'POST' ? JSON.stringify(req.body) : ''));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
   {
